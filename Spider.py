97868816df0e17756bb2f9105ae24f01f9715d99
Nikolay Kyorov <sc16nsk@leeds.ac.uk>
2019-03-07 14:53:01 +0000

    Robots.txt check

--- Spider.py
+++ Spider.py
@@ -3,16 +3,19 @@ import urllib.request
 import httplib2
 from urllib.parse import urljoin
 import tldextract
-
-# def get_domain(url):
-#     domain = tldextract.extract(url).registered_domain
-#     return domain
-
+import xml.etree.cElementTree as ET
+from reppy.robots import Robots
 
 urls_queue = set()
 crawled_urls = set()
 seed_url = "http://toscrape.com"
 seed_domain = tldextract.extract(seed_url).registered_domain
+
+
+def checkRobots(url):
+    robots = Robots.fetch(seed_url + '/robots.txt')
+    return robots.allowed(url,'*')
+
 
 def add_link(url):
     NOT_SEEN = True
@@ -36,11 +39,20 @@ def extract(page):
             
     for url in BeautifulSoup(response,parse_only=SoupStrainer('a'),features='html.parser'):
         if url.has_attr('href'):
-            url = urljoin(seed_domain,url['href'])
+            url = urljoin(page,url['href'])
             add_link(url)
 
 def crawl(page):
-    if page not in crawled_urls:
+    if page not in crawled_urls and checkRobots(page):
         extract(page)
         crawled_urls.add(page)
         urls_queue.discard(page)
+
+crawl(seed_url)
+
+
+url_copy = urls_queue.copy()
+while urls_queue:
+    for url in urls_queue.copy():
+        print("Crawling: " + url)    
+        crawl(url)
