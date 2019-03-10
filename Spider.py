97cfada619125d60f2e1b67a40a46034eb47a180
Nikolay Kyorov <sc16nsk@leeds.ac.uk>
2019-03-10 00:34:36 +0000

    Introduce a depth limit

--- Spider.py
+++ Spider.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import urllib.request
 import httplib2
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 import tldextract
 import xml.etree.cElementTree as ET
 from reppy.robots import Robots
@@ -15,12 +15,14 @@ class Spider:
     seed_domain = ''
     crawled = 0
     limit = 0
+    depth_limit = 0
     listToXML = []
 
-    def __init__(self,url,limit):
+    def __init__(self,url,limit,depth_limit):
         Spider.seed_url = Spider.urlChecker(url)
         Spider.seed_domain = tldextract.extract(self.seed_url).registered_domain
         Spider.limit = limit
+        Spider.depth_limit = depth_limit
         Spider.crawl(self.seed_url)
 
     def crawl(page):
@@ -35,7 +37,6 @@ class Spider:
                 Spider.createXML()
                 sys.exit("Maximum page limit reached!")
 
-
     def checkRobots(url):
         robots = Robots.fetch(Spider.seed_url + 'robots.txt')
         agent = robots.agent('*')
@@ -45,20 +46,20 @@ class Spider:
         else:
             return agent.allowed(url)
 
-
     def add_link(url,last_mod):
-        NOT_SEEN = True
-        SAME_DOMAIN = True
-        
+        SKIP = False        
         domain = tldextract.extract(url).registered_domain
 
-        if(url in Spider.urls_queue) and (url in Spider.crawled_urls):
-            NOT_SEEN = False
+        if url in Spider.crawled_urls:
+            SKIP = True
         
         if not domain == Spider.seed_domain:
-            SAME_DOMAIN = False
+            SKIP = True
+        
+        if Spider.getDepth(url) > Spider.depth_limit:
+            SKIP = True
 
-        if NOT_SEEN and SAME_DOMAIN:
+        if not SKIP:
             Spider.urls_queue.add(url)
             Spider.listToXML.append({'url':url,'last_mod':last_mod})
 
@@ -81,6 +82,10 @@ class Spider:
         else:
             url += '/'
             return url
+
+    def getDepth(url):
+        url = urlparse(url).path
+        return url.count('/') - 1
 
     def createXML():
         xml = open('sitemap.xml','w')
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Spider import *
 import time
 
 url = "http://toscrape.com"
-Spider(url,10)
+Spider(url,2000,2)
 
 while Spider.urls_queue:
     for link in Spider.urls_queue.copy():  
